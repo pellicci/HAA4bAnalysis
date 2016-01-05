bb12046505def6f14ebb1b4a418d0ae80f034057
Mario <mario.pellic@gmail.com>
2016-01-05 11:23:00 +0100

    adding a few files

--- test/crab_create_miniAOD.py
+++ test/crab_create_miniAOD.py
@@ -3,24 +3,25 @@ config = config()
 
 config.section_('General')
 config.General.transferOutputs = True
-config.General.requestName = 'HAA4b_Pythia8_AODSIM'
+config.General.requestName = 'HAA4b_Pythia8_miniAOD'
 config.General.workArea = 'crab_projects'
 
 config.section_('JobType')
-config.JobType.psetName = 'HAA4b_13TeV_pythia8_FULLSIM_cfg.py'
+config.JobType.psetName = 'miniAOD-prod_PAT.py '
 config.JobType.pluginName = 'PrivateMC'
-config.JobType.outputFiles = ['HAA4b_pythia8_FULLSIM.root']
-config.JobType.inputFiles = ['../../L1TriggerConfig/L1GtConfigProducers/data/Luminosity/startup/L1Menu_Collisions2015_25nsStage1_v5_L1T_Scales_20141121.xml']
+config.JobType.outputFiles = ['miniAOD-prod_PAT.root']
 
 config.section_('Data')
-config.Data.outputPrimaryDataset = 'HAA4b_AODSIM'
+config.Data.inputDataset = '/HAA4b/pellicci-HAA4b-b5eca7e9a51ba8c3ff68efa7a1e9c4f1/USER'
+config.Data.inputDBS = 'phys03'
+config.Data.outputPrimaryDataset = 'HAA4b_miniAOD'
 config.Data.splitting = 'EventBased'
 config.Data.unitsPerJob = 50
 NJOBS = 500
 config.Data.totalUnits = config.Data.unitsPerJob * NJOBS
 config.Data.outLFNDirBase = '/store/user/%s/' % (getUsernameFromSiteDB())
 config.Data.publication = True
-config.Data.outputDatasetTag = 'HAA4b_AODSIM'
+config.Data.outputDatasetTag = 'HAA4b_miniAOD'
 
 config.section_('Site')
 config.Site.storageSite = 'T2_IT_Legnaro'
