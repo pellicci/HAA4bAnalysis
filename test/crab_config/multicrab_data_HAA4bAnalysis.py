4c905f36de4a02dcb743caf4abe341eea097fa9f
Mario <mpel@protonmail.com>
2017-01-16 11:05:27 +0100

    trying to see Aashaq's commit

--- test/crab_config/multicrab_data_HAA4bAnalysis.py
+++ test/crab_config/multicrab_data_HAA4bAnalysis.py
@@ -11,7 +11,7 @@ config.JobType.psetName = 'run_HAA4bAnalysis.py'
 config.JobType.pluginName = 'Analysis'
 config.JobType.outputFiles = ['HAA4bAnalysis_output.root']
 config.JobType.pyCfgParams = ['runningOnData=True']
-config.JobType.allowUndistributedCMSSW = True
+#config.JobType.allowUndistributedCMSSW = True
 
 config.section_('Data')
 config.Data.lumiMask = 'json/Cert_271036-284044_13TeV_23Sep2016ReReco_Collisions16_JSON.txt'
@@ -43,7 +43,7 @@ if __name__ == '__main__':
 
     config.General.requestName = 'HAA4bAnalysis_BTagCSV_B'
     config.Data.inputDataset = '/BTagCSV/Run2016B-23Sep2016-v3/MINIAOD'
-    config.Data.unitsPerJob = 20
+    config.Data.unitsPerJob = 50
     from multiprocessing import Process
     p = Process(target=submit, args=(config,))
     p.start()
@@ -51,7 +51,7 @@ if __name__ == '__main__':
 
     # config.General.requestName = 'HAA4bAnalysis_BTagCSV_C'
     # config.Data.inputDataset = '/BTagCSV/Run2016C-23Sep2016-v1/MINIAOD'
-    # config.Data.unitsPerJob = 5
+    # config.Data.unitsPerJob = 50
     # from multiprocessing import Process
     # p = Process(target=submit, args=(config,))
     # p.start()
@@ -59,7 +59,7 @@ if __name__ == '__main__':
 
     # config.General.requestName = 'HAA4bAnalysis_BTagCSV_D'
     # config.Data.inputDataset = '/BTagCSV/Run2016D-23Sep2016-v1/MINIAOD'
-    # config.Data.unitsPerJob = 5
+    # config.Data.unitsPerJob = 50
     # from multiprocessing import Process
     # p = Process(target=submit, args=(config,))
     # p.start()
@@ -67,7 +67,7 @@ if __name__ == '__main__':
 
     # config.General.requestName = 'HAA4bAnalysis_BTagCSV_E'
     # config.Data.inputDataset = '/BTagCSV/Run2016E-23Sep2016-v1/MINIAOD'
-    # config.Data.unitsPerJob = 5
+    # config.Data.unitsPerJob = 50
     # from multiprocessing import Process
     # p = Process(target=submit, args=(config,))
     # p.start()
@@ -75,7 +75,7 @@ if __name__ == '__main__':
 
     # config.General.requestName = 'HAA4bAnalysis_BTagCSV_F'
     # config.Data.inputDataset = '/BTagCSV/Run2016F-23Sep2016-v1/MINIAOD'
-    # config.Data.unitsPerJob = 5
+    # config.Data.unitsPerJob = 50
     # from multiprocessing import Process
     # p = Process(target=submit, args=(config,))
     # p.start()
@@ -83,7 +83,7 @@ if __name__ == '__main__':
 
     # config.General.requestName = 'HAA4bAnalysis_BTagCSV_G'
     # config.Data.inputDataset = '/BTagCSV/Run2016G-23Sep2016-v1/MINIAOD'
-    # config.Data.unitsPerJob = 5
+    # config.Data.unitsPerJob = 50
     # from multiprocessing import Process
     # p = Process(target=submit, args=(config,))
     # p.start()
